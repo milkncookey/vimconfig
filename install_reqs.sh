01b8d397b9c26ac77a5eabf64fe1b741b853a3fc
Ruben Wu <ruben.wu@gmail.com>
2018-09-08 19:00:44 -0400

    Changed checking of prereq programs to  check if the program is already installed

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -uox pipefail
 
-sudo apt-get install python3
-sudo apt-get install build-essentials
-sudo apt-get install cmake
+# In case requirement programs are not installed yet
+if ! type "python3" > /dev/null; then
+    while true; do
+        read -p "Do you wish to install python3? [Y/n]
+" yn
+        case $yn in
+            Y ) sudo apt-get install python3; break;;
+            * ) echo "Continuing without installing python3"; break;;
+        esac
+    done
+fi
+if ! type "cmake" > /dev/null; then
+    while true; do
+        read -p "Do you wish to install cmake? [Y/n]
+" yn
+        case $yn in
+            Y ) sudo apt-get install cmake; break;;
+            * ) echo "Continuing without installing cmake"; break;;
+        esac
+    done
+fi
