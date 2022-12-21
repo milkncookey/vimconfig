8b49cf5cf66b0f7bab28651b349c9897efaf5b45
Ruben Wu <milkncookey@github.com>
2022-12-20 21:14:52 -0800

    Add additional dependancies for YCM module, remove options

--- install_reqs.sh
+++ install_reqs.sh
@@ -2,23 +2,6 @@
 set -uox pipefail
 
 # In case requirement programs are not installed yet
-if ! type "python3" > /dev/null; then
-    while true; do
-        read -p "Do you wish to install python3? [Y/n]
-" yn
-        case $yn in
-            Y ) sudo apt-get install python3; break;;
-            * ) echo "Continuing without installing python3"; break;;
-        esac
-    done
-fi
-if ! type "cmake" > /dev/null; then
-    while true; do
-        read -p "Do you wish to install cmake? [Y/n]
-" yn
-        case $yn in
-            Y ) sudo apt-get install cmake; break;;
-            * ) echo "Continuing without installing cmake"; break;;
-        esac
-    done
-fi
+sudo apt install -y python2-dev
+sudo apt install -y build-essential cmake python3-dev
+sudo apt install -y mono-complete golang nodejs default-jdk npm
